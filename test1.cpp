b9341a4979983dc650589d7a33bf801f75068253
root <anton@jesussaves.fun>
2018-03-03 09:03:30 +0500

    test

--- test1.cpp
+++ test1.cpp
@@ -3,35 +3,20 @@
 #include <string>
 #include "text.h"
 
-/***********************
- * B R O K E N         *
- * *********************/
 int main()
 {
-	std::string s = "abcdefgh\nABCDEFGH\n\nabcdefgh\nABCDEFGH\nabcdefgh\nABCDEFGH\nabcdefgh\nABCDEFGH\nabcdefgh\nABCDEFGH\n";
 	text<char> t;
-	t.assign(s.begin(), s.end());
-	assert(*t.begin() == 'a');
-	assert(t.line(t.begin()) == 0);
-	text<char>::iterator f = t.begin();
-	for (int i = 0; i < 9; i++) {
-		assert(t.line(f) == 0);
-		f++;
-	}
-	for (int i = 0; i < 9; i++) {
-		//std::cout << "!" << i << " " << t.line(f) << std::endl;
-		assert(t.line(f) == 1);
-		f++;
-	}
-	assert(t.line(f) == 2);
-	assert(std::distance(t.begin(), t.line(0)) == 0);
-	assert(*--f == '\n');
-	assert(t.line(f) == 1);
-	//std::cout << t.line(--t.end()) << std::endl;
-	assert(t.line(--t.end()) == 10);
-	assert(*t.line(0) == 'a');
-	assert(*t.line(1) == 'A');
-	assert(*t.line(2) == '\n');
-	assert(*t.line(3) == 'a');
-	t.check();
+	t.push_back('a');
+	t.push_back('b');
+	t.push_back('c');
+	t.marks["_"] = 3;
+	for (int i = 0; i < 5; i++)
+		t.insert('d' + i % 5);
+	auto j = t.begin();
+	assert(*j++ == 'a');
+	assert(*j++ == 'b');
+	assert(*j++ == 'c');
+	for (int i = 0; i < 10000; i++, j++)
+		assert(*j >= 'd');
+	assert(j == t.end());
 }
